13ced8f2db62d34cfc30b126214e08069e50df03
florent.haffner <florent.haffner@protonmail.com>
2020-05-13 19:59:23 +0200

    added forgotten stuff for VPS basic config

--- server.sh
+++ server.sh
@@ -1,7 +1,3 @@
-sudo apt install -y htop neovim zsh zip unzip
-source ~/.zshrc
-
-ssh-keygen -t rsa -b 4096 -C "florent.haffner@protonmail.com"
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
---
+++ vps.sh
@@ -0,0 +1,6 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y htop neovim zsh zip unzip
+source ~/.zshrc
+
+ssh-keygen -t rsa -b 4096 -C "florent.haffner@protonmail.com"
