a51fa39c46be2e8de0f4638578989361b5fa0f1c
florent.haffner <florent.haffner@protonmail.com>
2020-06-06 11:16:50 +0200

    update shell to install vim

---
+++ 3-vim.sh
@@ -0,0 +1,11 @@
+#
+# Plugins
+#
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+#
+# Recursive copy
+#
+cp -r $HOME/dotfiles/.config/nvim ~/.config
+
