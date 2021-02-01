8b17d19e2ea5f4026e5fe1890a6fd6e033b2cd58
Florent HAFFNER <florent.haffner@protonmail.com>
2021-02-01 11:28:19 +0100

    Update 2-server.sh

--- 2-server.sh
+++ 2-server.sh
@@ -1,10 +1,4 @@
 #
-# ZSH
-#
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-#
 # SDK (JVM)
 #
 curl -s "https://get.sdkman.io" | zsh
